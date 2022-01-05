3d21219314d8befa1e86084eb8f970fa3b7d3c1d
jorgevelasquezv <jorgeluisvelasquezv@gmail.com>
2022-01-04 20:16:23 -0500

    Primeros usos de sass

--- sass-react/src/App.jsx
+++ sass-react/src/App.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React from 'react';
 
 const App = () => {
-  return (
-    <div>
-      <h1>Sass</h1>
-    </div>
-  )
-}
+    return (
+        <div>
+            <h1 className="title">Sass</h1>
+            <hr />
+            <ul>
+                <li>
+                    <a href="#">Google</a>
+                </li>
+                <li>
+                    <a href="#">Facebook</a>
+                </li>
+                <li>
+                    <a href="#">Instagram</a>
+                </li>
+            </ul>
+        </div>
+    );
+};
 
-export default App
-
+export default App;
--- sass-react/src/index.js
+++ sass-react/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
+import './sass/index.scss';
 import App from './App.jsx';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
